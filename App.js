ec70683f874654c5ea0a2cf4d985528a6da2977a
theo-githu <111968864+theo-githu@users.noreply.github.com>
2023-06-04 17:22:31 +0100

    Async install

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // import { LogBox } from 'react-native';
 // LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
